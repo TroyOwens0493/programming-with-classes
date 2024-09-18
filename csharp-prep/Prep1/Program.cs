e2072b3183b80bd1b655f59483ea18cbbe5bce5d
Troy-Owens <troy@paywut.com>
2024-09-18 14:36:20 -0600

    Finished the first project.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,11 +1,13 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-        string answer;
-        answer = Console.ReadLine("Hi");
+class Program {
+    static void Main(string[] args) {
+        string first_name;
+        string last_name;
+        Console.Write("What is your first name? ");
+        first_name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        last_name = Console.ReadLine();
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
