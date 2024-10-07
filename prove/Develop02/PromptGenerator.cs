524edef704f7e2d9af39e0b7c117e353fdf93ca8
VenomusGraphics <cjwh2002@gmail.com>
2024-10-07 13:41:10 -0600

    changed to return prompt

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,16 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Xml.Serialization;
 
 class Program
 {
-    static void Main(string[] args)
+    static string Main(string[] args)
     {
         List<string> items = new List<string>
         {"potatos", "place holders go here"};
         Random random = new Random();
         int PromptIndex = random.Next(items.Count);
         string RandomPrompt = items[PromptIndex];
-        Console.WriteLine($"your prompt:"+RandomPrompt);
+        return RandomPrompt;
+        
     }
 }
 
