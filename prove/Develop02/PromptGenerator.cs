743d8d19c50ce973675759281a3b30e3e8de98a3
VenomusGraphics <cjwh2002@gmail.com>
2024-10-07 13:34:12 -0600

    for the promps with added places holders

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,10 +1,18 @@
 using System;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        List<string> items = new
+        { "potatos", "place holders go here"
+
+        };
+        Random random = new Random();
+        int PromptIndex = random.Next(items.Count);
+        string RandomPrompt = items[PromptIndex];
+        Console.WriteLine($"your prompt:"+RandomPrompt )
     }
 }
 
