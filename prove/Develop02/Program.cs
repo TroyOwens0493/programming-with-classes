d0f00b886832fa113663e55a3e5f9dd26681c082
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2024-10-07 13:45:35 -0600

    start work on main function

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,29 @@ using System;
 
 class Program
 {
+    // Attributes
+    string userSelection;
+
+    // Behaviors
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to your journal program!");
+        Console.WriteLine("Please choose what you would like to do.");
+        Console.WriteLine("1.Write");
+        Console.WriteLine("2.Display");
+        Console.WriteLine("3.Load");
+        Console.WriteLine("4.Save");
+        Console.WriteLine("5.Quit");
+        string userSelection = Console.ReadLine();
+        if ( userSelection = "1" ) {
+            string promptForUser = Entry.GetPrompt();
+            DateTime timeOfEntry = Entry.GetDateTime();
+            string userJournalEntry = Console.ReadLine();
+            string formattedEntry = formatJournalEntry(timeOfEntry, promptForUser, userJournalEntry);
+            Journal.AddEntries(formattedEntry);
+    }
+
+    static string formatJournalEntry(DateTime time_of_entry, string promptForUser, string journalEntry) {
+        DateTime.Parse(time_of_entry);
     }
 }
