e1227c0bd21a24da0a00a1e9875b14b997bedc67
VenomusGraphics <cjwh2002@gmail.com>
2024-10-09 12:53:04 -0600

    frm staric void to public

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -2,9 +2,9 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Xml.Serialization;
 
-class Prompt
+class Program
 {
-    static string Main(string[] args)
+    public string Main(string[] args)
     {
         List<string> items = new List<string>
         {"potatos", "place holders go here"};
