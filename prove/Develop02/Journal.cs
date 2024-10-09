ec1f9068e9a2de723cc44302d25b8ec7d94a61f5
Emma Loveless <georgetheawesomecat@gmail.com>
2024-10-09 13:31:01 -0600

    Wrote Journal.LoadFile()

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -22,7 +22,21 @@ public class Journal
     }
     public void LoadFile()
     {
+        Console.Write("What is the filename?");
+        string filename = Console.ReadLine();
 
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+            Entry newEntry = new Entry();
+        
+            newEntry._timestamp = parts[0];
+            newEntry._prompt = parts[1];
+            newEntry._body = parts[2];
+
+            _entries.Add(newEntry);
+        }
     }
     public void SaveFile()
     {
