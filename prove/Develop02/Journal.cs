823cc1e415cb96e015a0f31550904fbb77e94535
Daniel Loveless <danieltloveless@gmail.com.>
2024-10-11 11:17:31 -0600

    Removed header from save to fix load error

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -47,7 +47,6 @@ public class Journal
 
         using (StreamWriter outputFile = new StreamWriter(filename))
         {
-            outputFile.WriteLine("Datetime,Prompt,Response");
             foreach (Entry entry in _entries)
             {
                 outputFile.WriteLine($"{entry._timestamp}|{entry._prompt}|{entry._body}");
