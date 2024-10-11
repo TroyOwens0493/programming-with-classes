d9dcc3f479cf40f1712105de593db1ac3eee308a
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2024-10-11 11:19:41 -0600

    Revert "fiexed data loss bug" we found an easier way to do this This reverts commit f6af737579bcd6cb0856d3f3fa090afe4c9d05d4.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,7 +7,6 @@ class Program
     static void Main(string[] args)
     {
         bool exit = false;
-        Journal Journal = new();
         do
         {
             Console.WriteLine("Welcome to your journal program!");
@@ -19,6 +18,7 @@ class Program
             Console.WriteLine("5.Quit");
             string userSelection = Console.ReadLine();
             // Make a Journal obj
+            Journal Journal = new();
             if (userSelection == "1")
             {
                 Journal.AddEntry();
