086f2574732b250906bad566f73b6dd95b24c944
brysonprice <7bjprice@gmail.com>
2024-10-11 11:36:04 -0600

    added more journal prompts

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -10,8 +10,14 @@ class Prompts
         {"Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
-        "What was the strongest emotion I felt today?","If I had one thing I could do over today?", 
-        "what would it be?"};
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today what would it be?",
+        "What am I looking forward to this week?",
+        "How did I serve someone today?",
+        "What is love? (baby don't hurt me no more)",
+        "How did someone serve me today?",
+        "What am I grateful for today?",
+        "How have I felt God's love in my life today?"};
         Random random = new Random();
         int PromptIndex = random.Next(items.Count);
         string RandomPrompt = items[PromptIndex];
