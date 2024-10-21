e329c57ebd1de1a29113db5b19e2c10bfb8c07e7
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2024-10-21 14:12:54 -0600

    start work on the read-write class

---
+++ prove/Develop03/ReadWrite.cs
@@ -0,0 +1,15 @@
+class ReadWrite
+{
+    public List<String> GetScripture(string filename, string reference)
+    {
+        string line;
+        StreamReader sr = new StreamReader(filename);
+        line = sr.ReadLine();
+
+
+    }
+    public void WriteScripture(string reference, string verse, string filename)
+    {
+
+    }
+}
