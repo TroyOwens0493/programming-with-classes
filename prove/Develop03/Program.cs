5bcf77b486e047b12e10c568d9e4991dc9bc1323
brysonprice <7bjprice@gmail.com>
2024-10-21 13:26:41 -0600

    began program.cs work

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+
+        Scripture myScripture = new();
+        bool exit = false;
+
+        myScripture.Display();
+
+        do
+        {
+        Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+        string userInput = Console.ReadLine();
+        if (userInput == "")
+        {
+            myScripture.HideWords();
+            myScripture.Display();
+        }
+
+        else if (userInput == "quit")
+        {
+            exit = true;
+        }
+        } while (!exit);
+
     }
 }
