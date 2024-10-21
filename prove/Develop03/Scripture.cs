06b7c235ced788e0d6500d292069696c4c905735
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2024-10-21 13:25:44 -0600

    Add the reference class

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,4 +1,38 @@
+//Written by Troy
 class Scripture
 {
+    //Attributes
+    Reference _reference = new Reference();
+    List<Word> _words = new();
 
+    //Methods
+    public void Display()
+    {
+        foreach(word in _words)
+        {
+            word.Display()
+        }
+    }
+
+    public void HideWords(int numberOfWordsToHide)
+    {
+        int i = 0;
+        Random rnd = new Random();
+        do
+        {
+            Word wordToHide = _words[rnd.Next(_words.Count())];
+
+            if (wordToHide.GetIsHidden())//Pick a new word if the word is already hidden.
+            {
+                continue;
+            }
+            else//Hide the word
+            {
+                wordToHide.Hide();
+                i++;//Increment so we stop when the correct num of words are hidden.
+            }
+
+        } while (i < numberOfWordsToHide);
+
+    }
 }
