7ed6b06d213b3b1e19a03f18a68418592043e6b1
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2024-10-23 13:11:19 -0600

    add scripture getter

--- prove/Develop03/ReadWrite.cs
+++ prove/Develop03/ReadWrite.cs
@@ -1,15 +1,36 @@
 class ReadWrite
 {
-    public List<String> GetScripture(string filename, string reference)
+    public string GetScripture(string filename, string reference)
     {
         string line;
+        string cleanLine;
+        string scripture = "";
+        bool isScriptureLine = false;
         StreamReader sr = new StreamReader(filename);
         line = sr.ReadLine();
+        cleanLine = line.Trim();
+        if (line.StartsWith("REFERECE: "))
+        {
+            isScriptureLine = false;
+        }
 
+        if (line == $"REFERENCE: {reference}")
+        {
+            isScriptureLine = true;
+        }
+
+        if (isScriptureLine)
+        {
+            scripture += cleanLine.Replace("SCRIPTURE: ", "").Trim();
+        }
+
+        return scripture;
 
     }
+
     public void WriteScripture(string reference, string verse, string filename)
     {
 
     }
+
 }
