1ec242b4938affb9411a41ef8343b33ee51b41dd
TroyOwens0493 <TroyOwens0493@users.noreply.github.com>
2024-11-15 21:26:36 -0700

    finish the 05 learning activity

---
+++ prepare/Learning05/shape.cs
@@ -0,0 +1,24 @@
+abstract class Shape
+{
+    string _color;
+
+    public Shape(string color)
+    {
+        SetColor(color);
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
